6a4c934a9a9ca593499db1872c2fbd6abaf35264
wowufoundme <shubhkirti.work@gmail.com>
2019-06-20 03:30:07 +0530

    start:Add upgrading packages option

--- start.sh
+++ start.sh
@@ -7,21 +7,35 @@ CYAN='\033[0;36m'
 NC='\033[0m' 
 
 # Updating Environment
+printf "\n"
 read -p "Do you want to update the environment?(Y/N)" INP
 if [ "$INP" = "Y" ] || [ "$INP" = "y" ]
 then
-    printf "${CYAN}-----------------------------------\n      Updating environment \n----------------------------------- ${NC}\n"
+    printf "${CYAN}-----------------------------------\nUpdating environment \n----------------------------------- ${NC}\n"
     sudo apt-get update -y
 else
     echo "Skipping..."
 fi
 
+
 # Install build-essential
+printf "\n"
 read -p "Do you want to install build-essentials? (Y/N)" INP
 if [ "$INP" = "Y" ] || [ "$INP" = "y" ]
 then
-    printf "${CYAN}-----------------------------------\n      Installing build-essentials \n----------------------------------- ${NC}\n"
+    printf "${CYAN}-----------------------------------\nInstalling build-essentials \n----------------------------------- ${NC}\n"
     sudo apt-get install build-essential -y
+else
+    echo "Skipping..."
+fi
+
+# Upgrading Packages
+printf "\n"
+read -p "Do you want to upgrade software packages?(Y/N)" INP
+if [ "$INP" = "Y" ] || [ "$INP" = "y" ]
+then
+    printf "${CYAN}-----------------------------------\nUpdating environment \n----------------------------------- ${NC}\n"
+    sudo apt-get upgrade -y
 else
     echo "Skipping..."
 fi

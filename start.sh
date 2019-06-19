57f17779a67011c016293d0e8600db772cc3f19d
wowufoundme <shubhkirti.work@gmail.com>
2019-06-20 03:23:25 +0530

    start:Add build-essential

--- start.sh
+++ start.sh
@@ -11,7 +11,17 @@ read -p "Do you want to update the environment?(Y/N)" INP
 if [ "$INP" = "Y" ] || [ "$INP" = "y" ]
 then
     printf "${CYAN}-----------------------------------\n      Updating environment \n----------------------------------- ${NC}\n"
-    sudo apt-get update
+    sudo apt-get update -y
+else
+    echo "Skipping..."
+fi
+
+# Install build-essential
+read -p "Do you want to install build-essentials? (Y/N)" INP
+if [ "$INP" = "Y" ] || [ "$INP" = "y" ]
+then
+    printf "${CYAN}-----------------------------------\n      Installing build-essentials \n----------------------------------- ${NC}\n"
+    sudo apt-get install build-essential -y
 else
     echo "Skipping..."
 fi

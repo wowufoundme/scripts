85f9f2838a0ca1ab71decbbaee0fa33115cae9a6
wowufoundme <shubhkirti.work@gmail.com>
2019-06-20 03:19:39 +0530

    start:Add updating environment

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
+#! /bin/bash
 # A small script to run at the start of Ubuntu to initialize the
 # environment.
+
+# Set color variables
+CYAN='\033[0;36m'
+NC='\033[0m' 
+
+# Updating Environment
+read -p "Do you want to update the environment?(Y/N)" INP
+if [ "$INP" = "Y" ] || [ "$INP" = "y" ]
+then
+    printf "${CYAN}-----------------------------------\n      Updating environment \n----------------------------------- ${NC}\n"
+    sudo apt-get update
+else
+    echo "Skipping..."
+fi

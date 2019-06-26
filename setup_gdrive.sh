d6f4192b64ffa07d447e84fde4a204830af491c4
wowufoundme <shubhkirti.work@gmail.com>
2019-06-27 00:54:53 +0530

    gdrive:Add gdrive setup script

---
+++ setup_gdrive.sh
@@ -0,0 +1,5 @@
+cd ~
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+chmod +x gdrive
+sudo install gdrive /usr/local/bin/gdrive

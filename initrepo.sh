e7a40b70b9430305b4004632dbd61c9c0e97b4a1
wowufoundme <shubhkirti.work@gmail.com>
2019-06-28 04:42:49 +0530

    Minor Changes

--- initrepo.sh
+++ initrepo.sh
@@ -1,6 +1,6 @@
+PATH=~/bin:$PATH
 sudo rm -rf ~/bin/repo
-mkdir -p ~/bin
+mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 echo "Script Succeeded"
-PATH=~/bin:$PATH
